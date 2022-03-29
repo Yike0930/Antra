549201211d2646058ed3d5ea307182aa9f7cbefd
Yike Wang <yikewang@kais-mbp.lan>
2022-03-29 10:17:02 -0700

    homework1

---
+++ week3_homework/homework/src/main/java/entities/Student.java
@@ -0,0 +1,76 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Table(name = "student")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "stu", fetch = FetchType.LAZY , cascade = CascadeType.ALL, orphanRemoval = false)
+    private List<Student_Project> student_projects = new ArrayList<>();
+
+    public List<Student_Project> getStudent_Projects() {
+        return student_projects;
+    }
+
+    public void setStudent_Projects(List<Student_Project> sp) {
+        this.student_projects = sp;
+    }
+
+    public void addStudent_Projects(Student_Project sp) {
+        this.student_projects.add(sp);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+}
